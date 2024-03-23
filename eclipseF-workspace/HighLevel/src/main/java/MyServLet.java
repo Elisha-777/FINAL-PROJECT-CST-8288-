afa9a5a9cd75c81e35186eda20d41cf17cc3686a
Elisha-777 <kaly0012@algonquinlive.com>
2024-03-22 21:01:14 -0400

    second commit

--- eclipseF-workspace/HighLevel/src/main/java/MyServLet.java
+++ eclipseF-workspace/HighLevel/src/main/java/MyServLet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class MyServLet
- * ......
+ * ...............
  **/
 @WebServlet("/MyServLet")
 public class MyServLet extends HttpServlet {
@@ -27,6 +27,7 @@ public class MyServLet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
+    
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
